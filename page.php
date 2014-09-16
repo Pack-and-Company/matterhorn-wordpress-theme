dcec603641c477871ad782b41050938c7bc1f8f1
Tom Henderson <tomhenderson@mac.com>
2014-09-16 12:56:35 +1200

    Don't show the title on the home page.

--- page.php
+++ page.php
@@ -2,7 +2,14 @@
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-<div class="h3container" style="border-top: 4px solid black;"><h3><?=the_title();?></h3></div>
+<?php
+
+if (is_front_page()) {
+    ?>
+    <div class="h3container" style="border-top: 4px solid black;"><h3><?=the_title();?></h3></div>
+    <?php
+}
+?>
 
 <div id="textarea">
   <?php the_content(); ?>
